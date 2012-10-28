76b1d0752b51b2eb6344c3b6f0953ca08f326cfd
$imon <simon@kegtux.org>
2012-10-28 08:12:24 +0100

    Removed double connection, fixed some details

--- wget.php
+++ wget.php
@@ -1,5 +1,4 @@
 <?php
-
 // Wget for Symbiose WebOS
 // Coded by TiBounise (http://tibounise.com)
 // Released as GPL v3 software
@@ -15,26 +14,38 @@ if (!isset($ParsedUrl['host'])) {
   throw new InvalidArgumentException('URL invalide');
 }
 
-// Check if the file exists
-$fp = @fopen($Url,'r');
-if (!$fp) {
-  throw new InvalidArgumentException('URL inaccessible');
-}
-fclose($fp);
-
 // Dirty part to generate a new name for the file
 if (isset($ParsedUrl['path'])) {
   $baseFilename = end(explode('/',$ParsedUrl['path']));
 } else {
-  $baseFilename = $ParsedUrl['host'];
+  $baseFilename = 'index.html';
 }
-    
+
 $filename = $baseFilename;
 $i = 1;
-    
+
 while ($FileManager->exists($this->terminal->getAbsoluteLocation($filename))) {
-  $filename = $baseFilename . ' (' . $i . ')';
+  $filename = $baseFilename . '.' . $i;
   $i++;
 }
 
-$FileManager->createFile($this->terminal->getAbsoluteLocation($filename))->setContents(file_get_contents($Url));
+echo 'Connexion vers '.$ParsedUrl['host'].'...';
+// Check if the file exists
+$handle = @fopen($Url,'rb');
+if ($handle === false) {
+  throw new InvalidArgumentException('URL inaccessible');
+}
+echo ' connecté.'."\n";
+
+echo 'Sauvegarde en : « '.$filename.' »...'."\n";
+$contents = @stream_get_contents($handle);
+if ($contents === false) {
+	throw new InvalidArgumentException('Impossible de lire le fichier');
+}
+
+fclose($handle);
+
+$file = $FileManager->createFile($this->terminal->getAbsoluteLocation($filename));
+$file->setContents($contents);
+
+echo date('Y-m-d H:i:s').' - « '.$filename.' » sauvegardé ['.$file->size().']';
